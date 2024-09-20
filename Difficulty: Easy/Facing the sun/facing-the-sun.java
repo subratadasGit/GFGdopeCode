be2efe3d058e7d150b11f45f499f1bdb0763f0d8
Subrata Das <158165979+subratadasGit@users.noreply.github.com>
2024-09-20 21:40:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Facing the sun/facing-the-sun.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine(); // consume the remaining newline
+        while (t-- > 0) {
+            String input = sc.nextLine();
+            String[] inputStrings = input.split(" ");
+            int[] height = new int[inputStrings.length];
+            for (int i = 0; i < inputStrings.length; i++) {
+                height[i] = Integer.parseInt(inputStrings[i]);
+            }
+            Solution ob = new Solution();
+            int ans = ob.countBuildings(height);
+            System.out.println(ans);
+        }
+        sc.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Returns count buildings that can see sunlight
+    public int countBuildings(int[] height) {
+        // code here
+          int ans = 1, mx = height[0];
+        for(int x:height){
+            if(x>mx){
+                mx=x;
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
